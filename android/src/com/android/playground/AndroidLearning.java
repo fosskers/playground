dedb19acb6c7a1eb68d666d9161bb87eaf4e6d9e
Colin Woodbury <colingw@gmail.com>
2014-12-27 13:08:51 -0800

    (Android): Last commit before move

--- android/src/com/android/playground/AndroidLearning.java
+++ android/src/com/android/playground/AndroidLearning.java
@@ -18,16 +18,21 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.RadioButton;
 import java.util.List;
 
 // --- //
 
 public class AndroidLearning extends Activity
 {
+    public enum Strength { WEAK, MEDIUM, STRONG };
+    
     public final static String EXTRA_MSG = "com.android.playground.MESSAGE";
     private static final int NOTIFICATION_ID = 1;
     private static final int GENERIC_NOTIFICATION = 2;
+    private static Strength currStrength = Strength.WEAK;
     private static boolean service_on = false;
+    private static List<ScanResult> wifiFields = null;
 
     /** Called when the activity is first created. */
     @Override
@@ -40,16 +45,19 @@ public class AndroidLearning extends Activity
                     // Code to execute when the event occurs!
                     WifiManager wm = (WifiManager)c
                         .getSystemService(Context.WIFI_SERVICE);
-                    
                     List<ScanResult> rs = wm.getScanResults();  // <List>!
-
-                    genericNotification("Signals: " + rs.size());
-
                     LinearLayout ll = (LinearLayout)findViewById(R.id.masterLayout);
+
                     for(ScanResult sr : rs) {
                         TextView t = new TextView(c);
                         t.setText(sr.SSID + " " + sr.level);
                         ll.addView(t);
+                    }
+
+                    wifiFields = rs;
+
+                    if(inWifi()) {
+                        genericNotification("You are in a Wifi field!");
                     }
                 }
             }, i);
@@ -78,22 +86,13 @@ public class AndroidLearning extends Activity
 
     /* Turn functionality on/off and notify user */
     public void switchPress(View v) {
-        NotificationCompat.Builder b = new NotificationCompat.Builder(this);
-        b.setSmallIcon(R.drawable.ic_logo);
-        b.setAutoCancel(true);
-        b.setContentTitle(getResources().getString(R.string.notify_title));
-
         if(((Switch)v).isChecked()) {
-            b.setContentText(getResources().getString(R.string.notify_on));
+            // TODO: Make a scan here.
             service_on = true;
+            refresh(v);
         } else {
-            b.setContentText(getResources().getString(R.string.notify_off));
             service_on = false;
         }
-
-        NotificationManager nm =
-            (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        nm.notify(NOTIFICATION_ID, b.build());
     }
 
     public void refresh(View v) {
@@ -101,17 +100,76 @@ public class AndroidLearning extends Activity
 
         if(service_on) {
             wm = (WifiManager)getSystemService(WIFI_SERVICE);
-            genericNotification(
-                "Wifi on? " + String.valueOf(wm.isWifiEnabled())
-            );
             wm.startScan();
         }
 
         /* Eventually I'll need a WifiLock.
          * https://developer.android.com/reference/android/net/wifi/WifiManager.WifiLock.html
          * Use `acquire()` and `release()`.
-         * TODO: Find out about permissions, because this needs one.
          */
+    }
+
+    public void strengthSelected(View v) {
+        if(((RadioButton)v).isChecked()) {
+            switch(v.getId()) {
+            case R.id.radio_strong:
+                currStrength = Strength.STRONG;
+                break;
+            case R.id.radio_medium:
+                currStrength = Strength.MEDIUM;
+                break;
+            default:
+                currStrength = Strength.WEAK;
+                break;
+            }
+
+            refresh(v);
+        }
+    }
+
+    private Strength asStrength(int i) {
+        if(i < -90) {
+            return Strength.STRONG;
+        } else if(i < -50) {
+            return Strength.MEDIUM;
+        } else {
+            return Strength.WEAK;
+        }
+    }
+    
+    private boolean inWifi() {
+        for(ScanResult sr : wifiFields) {
+            if(currStrength == asStrength(sr.level)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /* Testing if user was in this field in the last scan */
+    private boolean alreadyInField(ScanResult c) {
+        for(ScanResult wf : wifiFields) {
+            if(c.SSID == wf.SSID) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /* Can trigger on two conditions:
+     * 1. A new SSID is detected with a Strength passing the threshold.
+     * 2. The strength of a field the user was in before passes the threshold.
+     */
+    private boolean enteredWifi(List<ScanResult> curr) {
+        for(ScanResult c : curr) {
+            // TODO: Check if `alreadyinField`.
+            // If so, are their strengths different? If so, is the curr
+            // one crossing the threshold? If so, notify and finish.
+        }
+
+        return false;
     }
 
     private void genericNotification(String msg) {
