5dc70b781b3cabdab7d315d40f8cc011ce63b355
Colin Woodbury <colingw@gmail.com>
2015-01-03 01:26:00 -0800

    (Haskell - Lens): More experimenting with my own Lenses
    - Well, Traversals really.

--- haskell/lens/pasta.hs
+++ haskell/lens/pasta.hs
@@ -2,20 +2,27 @@ module Pasta where
 
 import Control.Applicative
 import Control.Lens
+import Data.Monoid hiding ((<>))
 import Data.Semigroup
 
 ---
 
-data Food = F { calories :: Int
-              , fat      :: Int
-              , carbs    :: Int  -- Sugars!
-              , protein  :: Int } deriving (Eq,Show)
+data Food = F { calories :: Sum Int
+              , fat      :: Sum Int
+              , carbs    :: Sum Int  -- Sugars!
+              , protein  :: Sum Int } deriving (Eq,Show)
 
 instance Semigroup Food where
   F c1 f1 s1 p1 <> F c2 f2 s2 p2 = F (c1 + c2) (f1 + f2) (s1 + s2) (p1 + p2)
 
 foodLens :: Traversal' Food Int
-foodLens g (F c f s p) = F <$> g c <*> g f <*> g s <*> g p
+foodLens = fLens . sumLens
+
+fLens :: Traversal' Food (Sum Int)
+fLens g (F c f s p) = F <$> g c <*> g f <*> g s <*> g p
+
+sumLens :: Num a => Lens' (Sum a) a
+sumLens f (Sum s) = Sum <$> f s
 
 ---
 
