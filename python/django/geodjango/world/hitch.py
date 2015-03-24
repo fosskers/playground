2fc97f9428bd87d0bda336fd4b808358dc0a43ba
Colin Woodbury <colingw@gmail.com>
2015-03-23 22:06:47 -0700

    (Python - GeoDjango) `hitch` script updated to match requirements

--- python/django/geodjango/world/hitch.py
+++ python/django/geodjango/world/hitch.py
@@ -25,23 +25,27 @@ def timing(f):
 
 @timing
 def populate(num):
-    """Populates the DB with `num` Trips.
-    Trips here are randomly generated as any two origin/destination Points
-    on the Earth.
-    Range of latitude values:    (-90, 90)  => South/North from Equator
-    Range of longitutude values: (-180,180) => West/East from Greenwich
-
-    Possible improvements:
-      1. Only accept Point values that actually exist on a land mass.
-      2. Only restrict Long/Lat ranges to some radius around Vancouver.
-    """
+    """Populates the DB with `num` Trips."""
     print "Generating {} Trips".format(num)
     trips = imap(make_trip, xrange(0, num))
     Trip.objects.bulk_create(trips)
 
 
 def make_trip(n):
-    """Randomly generate a Trip. Ignore `n`"""
+    """Trips here are randomly generated as any two origin/destination Points
+    on the Earth.
+    Range of latitude values:    (-90, 90)  => South/North from Equator
+    Range of longitutude values: (-180,180) => West/East from Greenwich
+
+    The `Point` class in GeoDjango orders its coordinates opposite of
+    what is typical. The format is `Point(LONG,LAT)`.
+
+    Possible improvements:
+      1. Only accept Point values that actually exist on a land mass.
+      2. Only restrict Long/Lat ranges to some radius around Vancouver.
+
+    Note: The `n` value passed here is to be ignored.
+    """
     return Trip(
         origin=Point(uniform(-180.0, 180.0), uniform(-90.0, 90.0)),
         destination=Point(uniform(-180.0, 180.0), uniform(-90.0, 90.0)),
@@ -49,6 +53,7 @@ def make_trip(n):
     )
 
 
+@timing
 def clear_db():
     """Clears Trip DB."""
     print "Clearing {} entries from Trip table".format(Trip.objects.count())
@@ -57,14 +62,23 @@ def clear_db():
 
 
 @timing
-def find(point=Point(-123.6, 49.15), radius=20):
-    """Find all Trips which originate within `radius` kilometers of
-    some Point, which in practice would be the user's current position.
+def find_trip(origin=Point(-123.6, 49.15),
+              destination=Point(-122.57, 50.7),
+              radius=20):
+    """Find all 'relevant' Trips. A Trip is relevant if its origin is within
+    `radius` kilometers of a user specified origin, and its destination is
+    within `radius` kilometers of a user specified destination Point.
+
+    Resulting list is sorted by Trip origin distance to requested origin.
 
     Defaults:
-      point:  Vancouver
-      radius: 20km from `point`
+      origin:      Vancouver
+      destination: Whistler
+      radius:      20km search radius from both points
     """
-    return sorted(Trip.objects.filter(
-        origin__distance_lte=(point, D(km=radius))
-    ), key=lambda t: t.distance())
+    return sorted(
+        Trip.objects
+        .filter(origin__distance_lte=(origin, D(km=radius)))
+        .filter(destination__distance_lte=(destination, D(km=radius))),
+        key=lambda t: t.origin.distance(origin)
+    )
