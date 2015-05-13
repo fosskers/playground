1ac8663fb7bdfc7e304487c579c437c192142c73
Colin Woodbury <colingw@gmail.com>
2015-05-13 11:15:54 -0700

    (Dynamic) In hindsight, this code is pretty bad

--- haskell/Dynamic.hs
+++ haskell/Dynamic.hs
@@ -6,6 +6,8 @@ import Control.Lens
 import Data.Function (on)
 import Data.Maybe (fromJust)
 
+---
+
 type Weight = Int
 type Object = Int
 type Value  = Int
@@ -33,3 +35,29 @@ foo = [ (1,5,3)
       , (4,6,3)
       , (5,7,4)
       ]
+
+{-| What is the greatest common subsequence between two Strings?
+The characters of the subsequence must appear in the same order
+between the two Strings, but the characters themselves don't need
+to appear directly beside one another in the String.
+-}
+-- It would be nice to memoize this. State monad?
+-- We can cull branches. If we know the current Max across the whole tree,
+-- and we know the length of the current attempt,
+-- if `length globalMax > length curr + length (max of xs ys)`, then
+-- we don't have to recurse further, as this whole branch could never "win".
+-- 2015 March 25 @ 21:48 - Yes, this is the terrible way. O(2^n)
+subseqs :: String -> String -> String
+subseqs [] _ = []
+subseqs _ [] = []
+subseqs (x:xs) (y:ys)
+  | x == y    = x : subseqs xs ys
+  | otherwise = fromJust $ maximumByOf both (compare `on` length) (a,b)
+    where a = subseqs xs (y:ys)
+          b = subseqs (x:xs) ys
+
+bar :: String
+bar = "ABCBDABDABCDABADCBAD"
+
+baz :: String
+baz = "BDCABBACDBADCBABCBCD"
