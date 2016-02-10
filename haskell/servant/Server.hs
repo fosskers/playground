1919c157abf09b0b9414738073dd536c8fafb1fc
Colin Woodbury <colingw@gmail.com>
2016-02-09 16:18:48 -0800

    (Haskell - servant) Basic server example

--- haskell/servant/Server.hs
+++ haskell/servant/Server.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -7,17 +8,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Server where
 
 import           Data.Aeson
-import qualified Data.Aeson.Parser
-import           Data.Aeson.Types
-import           Data.Attoparsec.ByteString
-import           Data.ByteString (ByteString)
-import           Data.Int
 import           Data.List
-import           Data.String.Conversions
 import           Data.Time.Calendar
 import           GHC.Generics
 import           Lucid
@@ -25,10 +21,6 @@ import           Network.HTTP.Media
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
-import           System.Directory
-import           Text.Blaze
-import qualified Text.Blaze.Html
-import           Text.Blaze.Html.Renderer.Utf8
 
 ---
 
@@ -43,7 +35,7 @@ data User = User
 
 instance ToJSON Day where
     -- display a day in YYYY-mm-dd format
-    toJSON d = toJSON (showGregorian d)
+    toJSON = toJSON . showGregorian
 
 instance ToJSON User
 
@@ -51,14 +43,111 @@ users :: [User]
 users = [ User "Colin W" 27 "colingw@gmail.com" (fromGregorian 1988 7 5)
         , User "Jack" 2 "jack@cat.com" (fromGregorian 2013 5 31) ]
 
-server :: Server UserApi
-server = pure users
-
 userApi :: Proxy UserApi
 userApi = Proxy
 
+{-}
+server :: Server UserApi
+server = pure users
+
 app :: Application
 app = serve userApi server
-
+-}
 main :: IO ()
 main = run 8081 app
+
+---
+
+type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
+  :<|> "hello" :> QueryParam "name" String :> Get '[JSON] Hi
+  :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
+
+data Position = Position { x :: Int, y :: Int } deriving Generic
+
+instance ToJSON Position
+
+newtype Hi = Hi { msg :: String } deriving Generic
+
+instance ToJSON Hi
+
+data ClientInfo = ClientInfo
+  { clientName :: String
+  , clientEmail :: String
+  , clientAge :: Int
+  , clientInterestedIn :: [String]
+  } deriving Generic
+
+instance FromJSON ClientInfo
+instance ToJSON ClientInfo
+
+data Email = Email
+  { from :: String
+  , to :: String
+  , subject :: String
+  , body :: String
+  } deriving Generic
+
+instance ToJSON Email
+
+emailForClient :: ClientInfo -> Email
+emailForClient c = Email from' to' subject' body'
+  where from' = "great@company.com"
+        to'   = clientEmail c
+        subject' = "Hey " ++ clientName c ++ ", we miss you!"
+        body'    = "Hi " ++ clientName c ++ ",\n\n"
+          ++ "Since you've recently turned " ++ show (clientAge c)
+          ++ ", have you checked out our latest "
+          ++ intercalate ", " (clientInterestedIn c)
+          ++ " products? Give us a visit!"
+
+eServer :: Server API
+eServer = position :<|> hi :<|> marketing
+  where position x y = pure $ Position x y
+        hi mname = pure . Hi $ "Hi, " ++ maybe "coward" id mname
+        marketing = pure . emailForClient
+
+---
+
+data HTMLLucid
+
+instance Servant.Accept HTMLLucid where
+  contentType _ = "text" // "html" /: ("charset", "utf-8")
+
+instance ToHtml a => MimeRender HTMLLucid a where
+  mimeRender _ = renderBS . toHtml
+
+instance MimeRender HTMLLucid (Html a) where
+  mimeRender _ = renderBS
+
+type PersonAPI = "persons" :> Get '[JSON, HTMLLucid] [Person]
+
+data Person = Person { firstName :: String, lastName :: String } deriving Generic
+
+instance ToJSON Person
+
+instance ToHtml Person where
+  toHtml person = tr_ $ do
+    td_ (toHtml $ firstName person)
+    td_ (toHtml $ lastName person)
+
+  toHtmlRaw = toHtml
+
+instance ToHtml [Person] where
+  toHtml ps = table_ $ do
+    tr_ $ th_ "first name" *> th_ "last name"
+    foldMap toHtml ps
+
+  toHtmlRaw = toHtml
+
+persons :: [Person]
+persons = [ Person "Colin" "Woodbury"
+          , Person "Jack" "Catworthy" ]
+
+personAPI :: Proxy PersonAPI
+personAPI = Proxy
+
+server :: Server PersonAPI
+server = pure persons
+
+app :: Application
+app = serve personAPI server
