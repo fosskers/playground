7907923a9c49103da244f33f88c9c9d51ea014be
Colin Woodbury <colingw@gmail.com>
2016-02-11 23:39:14 -0800

    (Haskell - servant) Works with `extensible-effects`!

--- haskell/servant/ExtensibleEffects.hs
+++ haskell/servant/ExtensibleEffects.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
@@ -11,8 +12,10 @@ import           Control.Eff.Lift
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Either
 import           Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as B
 import           Data.Void
 import           GHC.Generics
+import           Lens.Micro
 import           Network.Wai (Application)
 import qualified Network.Wai.Handler.Warp as W
 import           Servant
@@ -29,13 +32,14 @@ type Effect = Eff (Exc String E.:> Lift IO E.:> Void)
 -- and the default `EitherT ServantErr IO` that servant uses.
 effToEither' :: Effect a -> EitherT ServantErr IO a
 effToEither' eff = liftIO (runLift $ runExc eff) >>= f
-  where f = either (\e -> left err404 { errReasonPhrase = e }) right
+  where f = either (\e -> left err404 { errBody = B.pack e }) right
 
 -- | The Natural Transformation
 effToEither :: Effect :~> EitherT ServantErr IO
 effToEither = Nat effToEither'
 
 type API = "cats" :> Get '[JSON] [Cat]
+           :<|> "cat" :> QueryParam "name" String :> Get '[JSON] Cat
 
 data Cat = Cat { name :: String, collar :: Bool } deriving Generic
 
@@ -48,11 +52,29 @@ cats = [ Cat "Jack" True
        , Cat "Pip" False
        , Cat "Qtip" True ]
 
+catNoises :: SetMember Lift (Lift IO) r => Eff r ()
+catNoises = lift $ putStrLn "Meow meow."
+
+-- | Here we see nice mixing of two effects, Exc and IO.
+-- Notice we also call "child" effects (which claim less effects than
+-- the whole set) with no overhead.
+catLookup :: Maybe String -> Effect Cat
+catLookup Nothing = throwExc "No cat name given!"
+catLookup (Just n) = do
+  case cats ^? each . filtered ((== n) . name) of
+    Nothing -> throwExc $ "There is no cat named " ++ n ++ "!"
+    Just c -> (lift . putStrLn $ "Found cat: " ++ n) *> catNoises *> pure c
+
+-- | A server using our custom handler Monad.
+serverT :: ServerT API Effect
+serverT = pure cats :<|> catLookup
+
+-- | Our `ServerT` mapped backed to a vanilla `Server` via the N.T. above.
+server :: Server API
+server = enter effToEither serverT
+
 api :: Proxy API
 api = Proxy
-
-server :: Server API
-server = pure cats
 
 app :: Application
 app = serve api server
