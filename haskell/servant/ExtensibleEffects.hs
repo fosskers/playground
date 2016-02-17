e4a7f1cf705057799ed804184955f7943dff7d6e
Colin Woodbury <colingw@gmail.com>
2016-02-16 18:22:46 -0800

    (Haskell - servant) Can autogenerate API docs

--- haskell/servant/ExtensibleEffects.hs
+++ haskell/servant/ExtensibleEffects.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module ExtensibleEffects where
 
@@ -13,12 +17,16 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Either
 import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Text.Lazy as TL
+import           Data.Text.Lazy.Encoding (encodeUtf8)
 import           Data.Void
 import           GHC.Generics
 import           Lens.Micro
-import           Network.Wai (Application)
+import           Network.HTTP.Types
+import           Network.Wai (Application, responseLBS)
 import qualified Network.Wai.Handler.Warp as W
 import           Servant
+import           Servant.Docs
 
 ---
 
@@ -38,12 +46,24 @@ effToEither' eff = liftIO (runLift $ runExc eff) >>= f
 effToEither :: Effect :~> EitherT ServantErr IO
 effToEither = Nat effToEither'
 
-type API = "cats" :> Get '[JSON] [Cat]
-           :<|> "cat" :> QueryParam "name" String :> Get '[JSON] Cat
+type CatAPI = "cats" :> Get '[JSON] [Cat]
+              :<|> "cat" :> QueryParam "name" String :> Get '[JSON] Cat
+
+instance ToParam (QueryParam "name" String) where
+  toParam _ = DocQueryParam "name"
+    ["Jack","Miso","Turbo","Pip","Qtip"]
+    "Lookup cat info by their name."
+    Normal
 
 data Cat = Cat { name :: String, collar :: Bool } deriving Generic
 
 instance ToJSON Cat
+
+instance ToSample Cat Cat where
+  toSample _ = Just (Cat "Jack" True)
+
+instance ToSample [Cat] [Cat] where
+  toSample _ = Just ([Cat "Jack" True, Cat "Miso" False])
 
 cats :: [Cat]
 cats = [ Cat "Jack" True
@@ -59,21 +79,29 @@ catNoises = lift $ putStrLn "Meow meow."
 -- Notice we also call "child" effects (which claim less effects than
 -- the whole set) with no overhead.
 catLookup :: Maybe String -> Effect Cat
-catLookup Nothing = throwExc "No cat name given!"
+catLookup Nothing = throwExc ("No cat name given!" :: String)
 catLookup (Just n) = do
   case cats ^? each . filtered ((== n) . name) of
     Nothing -> throwExc $ "There is no cat named " ++ n ++ "!"
     Just c -> (lift . putStrLn $ "Found cat: " ++ n) *> catNoises *> pure c
 
 -- | A server using our custom handler Monad.
-serverT :: ServerT API Effect
+serverT :: ServerT CatAPI Effect
 serverT = pure cats :<|> catLookup
 
--- | Our `ServerT` mapped backed to a vanilla `Server` via the N.T. above.
-server :: Server API
-server = enter effToEither serverT
+type DocsAPI = CatAPI :<|> Raw
 
-api :: Proxy API
+-- | Our `ServerT` mapped backed to a vanilla `Server` via the N.T. above.
+server :: Server DocsAPI
+server = enter effToEither serverT :<|> serveDocs
+  where serveDocs _ respond = respond $ responseLBS ok200 [plain] docsBS
+        plain = ("Content-Type", "text/plain")
+
+docsBS :: B.ByteString
+docsBS = encodeUtf8 . TL.pack . markdown $ docsWithIntros [intro] api
+  where intro = DocIntro "Cat API" ["The only API the internet needs."]
+
+api :: Proxy DocsAPI
 api = Proxy
 
 app :: Application
