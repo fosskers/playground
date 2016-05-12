113af1e9d27c8e446dd096611a778cbd25749b6f
Colin Woodbury <colingw@gmail.com>
2016-05-12 12:26:54 -0700

    (Scala - Diode) React components work

--- scala/diode/full/build.sbt
+++ scala/diode/full/build.sbt
@@ -2,7 +2,7 @@
 enablePlugins(ScalaJSPlugin)
 
 // Use NodeJS
-//scalaJSUseRhino in Global := false
+scalaJSUseRhino in Global := false
 
 // To avoid manually launching the app in HTML
 persistLauncher in Compile := true
@@ -16,9 +16,26 @@ scalaVersion := "2.11.8"
 
 // Change this to another test framework if you prefer
 libraryDependencies ++= Seq(
+  //  "com.github.japgolly.scalajs-react" %%% "core" % "0.10.4",
+  "com.github.japgolly.scalajs-react" %%% "extra" % "0.10.4",
   "org.scala-js" %%% "scalajs-dom" % "0.9.0",
   "me.chrons" %%% "diode" % "0.5.1",
   "me.chrons" %%% "diode-react" % "0.5.1"
+)
+
+jsDependencies ++= Seq(
+  RuntimeDOM,
+
+  "org.webjars.bower" % "react" % "15.0.1"
+    /        "react-with-addons.js"
+    minified "react-with-addons.min.js"
+    commonJSName "React",
+
+  "org.webjars.bower" % "react" % "15.0.1"
+    /         "react-dom.js"
+    minified  "react-dom.min.js"
+    dependsOn "react-with-addons.js"
+    commonJSName "ReactDOM"
 )
 
 // Uncomment to use Akka
--- scala/diode/full/src/main/scala/com/example/React.scala
+++ scala/diode/full/src/main/scala/com/example/React.scala
@@ -1,52 +1,104 @@
 package com.example
 
-import diode.{ ActionHandler, Circuit, Effect, ModelRW }
-import diode.react.ReactConnector
-import org.scalajs.dom
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.scalajs.js.JSApp
+
+import diode.{ActionHandler, Circuit, Effect, ModelRW}
+import diode.react.{ModelProxy, ReactConnector}
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.prefix_<^._
+import org.scalajs.dom
 
 // --- //
 
 /* Model Tree */
-//case class MsgDB(users: Seq[User])
-//case class User(id: String, msgs: Seq[String])
 case class DBRoot(db: MsgDB)
 case class MsgDB(msgs: Seq[String], logSize: Int)
 
 /* Actions */
 case class Post(msg: String)
+case object Reset
 case class LogIt(msg: String)
 case object Logged
-//case class NewUser(user: String)
 
-object ReactApp extends Circuit[DBRoot] with ReactConnector[DBRoot] {
-  override def initialModel = DBRoot(MsgDB(Seq(), 0))
+object ReactCircuit extends Circuit[DBRoot] with ReactConnector[DBRoot] {
+  override def initialModel = DBRoot(MsgDB(Seq("Hi Nathan"), 0))
 
   // Boilerplate :(
-  val dbL: ModelRW[DBRoot, MsgDB] = zoomRW(_.db)((m,v) => m.copy(db = v))
-  val msgL: ModelRW[DBRoot, Seq[String]] = dbL.zoomRW(_.msgs)((m,v) => m.copy(msgs = v))
-  val lsL: ModelRW[DBRoot, Int] = dbL.zoomRW(_.logSize)((m,v) => m.copy(logSize = v))
+  val dbL: ModelRW[DBRoot, MsgDB] = zoomRW(_.db)((m, v) => m.copy(db = v))
+  val msgL: ModelRW[DBRoot, Seq[String]] = dbL.zoomRW(_.msgs)((m, v) => m.copy(msgs = v))
+  val lsL: ModelRW[DBRoot, Int] = dbL.zoomRW(_.logSize)((m, v) => m.copy(logSize = v))
+
+  val resetH = new ActionHandler(dbL) {
+    override def handle = {
+      case Reset => updated(MsgDB(Seq(), 0))
+    }
+  }
 
   val postH = new ActionHandler(msgL) {
     override def handle = {
-      case Post(m) => updated(value :+ m, Effect.action(LogIt))
+      case Post(m) => {
+        updated(value :+ m, Effect.action(LogIt(m)))
+      }
     }
   }
 
   val logH = new ActionHandler(lsL) {
     override def handle = {
       case LogIt(m) => effectOnly(logIt(m) >> logIt(m :+ '!'))
-      case Logged => updated(value + 1)
+      case Logged => {
+        println(value)
+        updated(value + 1)
+      }
     }
   }
 
   /* This is the main handler */
-  override val actionHandler = composeHandlers(postH, logH)
+  override val actionHandler = composeHandlers(resetH, postH, logH)
 
   /* This could be any Future effect anywhere */
   def logIt(m: String) = Effect(Future {
     println(m)
     Logged
   })
+}
+
+object ReactView {
+
+  val top = ReactComponentB[ModelProxy[MsgDB]]("TopComponent").render_P({ proxy =>
+    <.div(
+      proxy.wrap({ db: MsgDB => db })(inputThing(_)),
+      proxy.connect({ db: MsgDB => db })(page(_))
+    )
+  }).build
+
+  val page = ReactComponentB[ModelProxy[MsgDB]]("MessageDisplay").render_P({ proxy =>
+    <.div(
+      proxy().logSize,
+      proxy().msgs.map(m => <.div(m))
+    )
+  }).build
+
+  val inputThing = ReactComponentB[ModelProxy[MsgDB]]("Input").render_P({ proxy =>
+    <.div(
+      <.input(
+        ^.tpe := "input",
+        ^.onChange ==> { e: ReactEventI => proxy.dispatch(Post(e.target.value)) }
+      ),
+      <.button(
+        ^.onClick --> proxy.dispatch(Reset),
+        "Reset"
+      )
+    )
+  }).build
+}
+
+object ReactDiode extends JSApp {
+  def main(): Unit = {
+    ReactDOM.render(
+      ReactCircuit.connect(_.db)(ReactView.top(_)),
+      dom.document.getElementById("reactdiode")
+    )
+  }
 }
