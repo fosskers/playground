8309d7e7909be849f1f18683518227efca2b2fa2
Colin Woodbury <colingw@gmail.com>
2016-06-08 13:00:54 -0700

    (Haskell - pipes) Some aeson stuff

--- haskell/pipes/Streaming.hs
+++ haskell/pipes/Streaming.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Streaming where
 
 import Pipes
 import Pipes.Parse
 import Pipes.Aeson
+import qualified Pipes.Aeson.Unchecked as A
 import qualified Pipes.Prelude as P
 import qualified Pipes.ByteString as P
 import Data.ByteString (ByteString)
-import Data.Aeson (Value)
+import Data.Aeson hiding (decode)
+import Lens.Micro hiding (each)
+import Lens.Micro.Aeson
+import Lens.Micro.Mtl
+import Data.Functor.Identity
 
 ---
 
@@ -68,6 +74,7 @@ greet = do
 {- ListT -}
 
 -- | The List Monad transformer "done right".
+-- Use `every` to turn a `ListT` into a `Producer`.
 cross :: Monad m => ListT m Int
 cross = (*) <$> Select (each [1..3]) <*> Select (each [1..3])
 
@@ -81,11 +88,24 @@ drawThree = (,,) <$> draw <*> draw <*> draw
 
 -- This appears to be a library of primatives for defining parsers,
 -- rather than a library which does complex text parsing.
+-- `pipes-attoparsec` probably does that.
 
 {- AESON -}
 
 bytes :: Monad m => Producer ByteString m ()
-bytes = yield "{ \"fun\": 4 }"
+bytes = do
+  yield "{"
+  yield " \"fun\": 4,"
+  yield " \"joy\": 5,"
+  yield " \"happy\": 6"
+  yield "}"
+  yield "1"
 
-res :: IO (Maybe (Either DecodingError Value))
-res = evalStateT decode bytes
+-- | The entire Producer is consumed, despite my tricks.
+res :: (Maybe Double, [ByteString])
+res = (r',p')
+  where (r,p) = runIdentity $ runStateT decode bytes
+        p' = P.toList p
+        r' = r ^? _Just . _Right . key @Value "fun" . _Double
+
+-- Looks like the `json-stream` package does what I want.
