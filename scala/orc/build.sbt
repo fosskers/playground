0b9064b73b6e1f2dec80896d56cc5280586f8d89
Colin Woodbury <colingw@gmail.com>
2017-07-11 13:42:41 -0700

    (Scala - ORC) Can parse all element types

--- scala/orc/build.sbt
+++ scala/orc/build.sbt
@@ -2,10 +2,10 @@ name := """orc"""
 
 version := "1.0.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.11.11"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark"            %% "spark-core"            % "2.1.0",
-  "org.apache.spark"            %% "spark-hive"            % "2.1.0",
-  "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+  "org.apache.spark" %% "spark-core" % "2.1.0",
+  "org.apache.spark" %% "spark-hive" % "2.1.0",
+  "org.scalatest"    %% "scalatest"  % "2.2.4" % "test"
 )
--- scala/orc/src/main/scala/playground/Orc.scala
+++ scala/orc/src/main/scala/playground/Orc.scala
@@ -1,14 +1,18 @@
 package playground
 
 import org.apache.spark._
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.apache.spark.sql.hive._
+import org.apache.log4j.{Level, Logger}
 
 // --- //
 
 /* Weird, `Map` must be typed in this verbose way */
 case class Node(id: Long, tags: scala.collection.Map[String, String], lat: BigDecimal, lon: BigDecimal)
 case class Way(id: Long, tags: scala.collection.Map[String, String], nds: Seq[Long])
+case class Relation(id: Long, tags: scala.collection.Map[String, String], members: Seq[Member])
+case class Member(`type`: String, ref: Long, role: String)
 
 object Orc extends App {
   override def main(args: Array[String]): Unit = {
@@ -20,35 +24,50 @@ object Orc extends App {
     implicit val sc: SparkContext = new SparkContext(conf)
     implicit val hc: HiveContext = new HiveContext(sc)
 
+    /* Magic. Things don't compile without this. */
     import hc.sparkSession.implicits._
+
+    /* Silence the damn INFO logger */
+    Logger.getRootLogger().setLevel(Level.ERROR)
 
     /* Necessary for "predicate push-down" */
     hc.setConf("spark.sql.orc.filterPushdown", "true")
 
-    /* A reader? */
-    val data = hc.read.format("orc").load("VA.orc")
+    /* A lazy reader of some sort */
+    val data: DataFrame = hc.read.format("orc").load("uku.orc")
 
-    val nodes =
+    val nodes: RDD[Node] =
       data
         .select("id", "tags", "lat", "lon")
         .where("type = 'node'")
         .as[Node]
         .rdd
 
-    val ways =
+    /* There seems to be some runtime reflection that lets the results
+     * actually parse into a `Way`. Notice there aren't any explicit typeclass
+     * instances defined anywhere.
+     */
+    val ways: RDD[Way] =
       data
         .select($"id", $"tags", $"nds.ref".alias("nds"))
         .where("type = 'way'")
         .as[Way]
         .rdd
 
-    /* `println(res)` prints something odd */
-    //    res.show()
-    println(s"NODES: ${nodes.count()}")
-    println(s"WAYS: ${ways.count()}")
-//    res.foreach(println)
+    val relations: RDD[Relation] =
+      data
+        .select($"id", $"tags", $"members")  // Why are the `$` necessary?
+        .where("type = 'relation'")
+        .as[Relation]
+        .rdd
 
-    /* Safely stop the Spark Context */
-    sc.stop()
+    try {
+      println(s"NODES: ${nodes.count()}")
+      println(s"WAYS: ${ways.count()}")
+      println(s"RELATIONS: ${relations.count()}")
+    } finally {
+      /* Safely stop the Spark Context */
+      sc.stop()
+    }
   }
 }
